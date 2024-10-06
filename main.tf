160d574863e9a62a5f416889b7defbf87c8b2505
syedd <smdastagir@gmail.com>
2024-10-06 11:52:28 +0100

    Update

---
+++ main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "eu-west-2" # London region
+}
+resource "aws_s3_bucket" "terra-form-augustproject123" {
+  bucket = "terraformaugustproject123"
+}
+terraform {
+  backend "s3" {
+    # Replace this with your bucket name!
+    bucket         = "terraformaugustproject123"
+    key            = "global/s3/terraform.tfstate"
+    region         = "eu-west-2"
+  }
+}
